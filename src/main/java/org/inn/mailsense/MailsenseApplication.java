a77db6b406a2f3e087a55f8933a49428e6bb4755
Tushar Nagdive <codewithtushar840@gmail.com>
2025-11-08 21:32:34 +0530

    mailsense code

---
+++ src/main/java/org/inn/mailsense/MailsenseApplication.java
@@ -0,0 +1,13 @@
+package org.inn.mailsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailsenseApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(MailsenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/inn/mailsense/MailsenseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inn.mailsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailsenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
